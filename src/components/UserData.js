f7523a8502b2b5ae140785ff559459970cae536a
cchackathon <employerrelationscoc@gmail.com>
2021-04-13 11:19:31 -0400

    added real user data

--- src/components/UserData.js
+++ src/components/UserData.js
@@ -1,37 +1,33 @@
 export const UserData = [
     {
         id: 0,
-        name: 'Name1',
-        major: 'Major',
-        year: 'Year at Tech',
-        quote: 'Quote',
-        job: 'Job at Tech',
-        welcome: 'Welcome Message',
-        q1: 'words1',
-        q2: 'words2',
-        q3: 'words3',
-        q4: 'words4',
-        q5: 'words5',
-        q6: 'words6',
-        q7: 'words7',
+        name: 'Marissa Yun',
+        major: 'Computer Science',
+        year: '3rd Year',
+        quote: 'Welcome to Georgia Tech! ',
+        job: 'Student Assistant at College of Computing Career Services',
+        welcome: 'Welcome to Georgia Tech! Here you will find so many opportunities so make the most of them! Career Services hosts a lot of useful career workshops and what I would consider to be the best career fair at Georgia Tech! There are also so many different clubs, not only CS related, but cultural, social, academic, and so much more so I encourage you to explore them. While being a woman at Tech can be overwhelming sometimes, especially in a male-dominated field like computer science, there is a strong network of women at the college of computing who will always be there for you and to help you! I hope you all enjoy your time here since these will be some of the best years of your life!',
+        q1: "My first internship was in New Zealand at a small fintech startup through Georgia Tech's Global Internships program! I learned a wide variety of tools and skills that I hadn't had an experience with yet as a first year like Git, TypeScript, databases, product design, how sprints work, and so much more! I also gained fluency in working in a new culture and work environment along the way. My second internship was at NCR in Atlanta last summer! Unfortunately, it was remote, but I gained a lot of valuable experience working on the Fuel Team like working with APIs and data parsing. My internship this summer will be at Amazon as a Propel Program intern! I hope to gain valuable experience through the first time Amazon is running this program!",
+        q2: 'What inspires me to continue in computer science is how there are so many fields to choose from, whether its product management, software engineers, design, data science, and so much more! Technology will always be changing and I want to be constantly learning and growing. I believe technology can truly make a difference in the world and that I can help be a part of that change!',
+        q3: 'There are also so many different clubs, not only CS related, but cultural, social, academic, and so much more so I encourage you to branch out and explore them! Take advantage of day in the lobbies, tech talks, career fairs, and career services workshops since they will only help you and I have had opportunities arise from going to these events! While being a woman at Tech can be overwhelming sometimes, especially in a male-dominated field like computer science, there is a strong network of women at the college of computing who will always be there for you and to help you!',
+        q4: 'I wish that I would have applied to all of the first-year programs that a lot of companies have but I was unaware of them. I also wish that I took less credit hours and took the time to relax and enjoy campus life while course load was still easier.',
+        q5: "W@CC (Women at College of Computing) was one of the first clubs I joined at Tech and while I'm not as active now, it's where I found some of my friends but also expanded my network. Within TA office hours! For any CoC class, there's bound to be other students there also needing help especially when TAs are overwhelmed with students. Offer to help and people will also offer to help you! I mainly had other female CS students reach out to me since sometimes fellow women just feel easier to approach. Career Services! So many leaders in CoC Career Services are women and while they may not have majored in CS, they are still very knowledgable after having worked with CS students for many years and can offer valuable professional advice",
+        q6: 'Career Services events (Tech Talks, Days in the Lobby, CoC Career Fair, Career Services Workshops), CoC advisors (for planning schedules, general advice), Office of International Education (study abroad/intern abroad), All-Campus Career Fair, follow instagram pages (coc_careers) and read Career Services emails for opportunities, W@CC newsletter and events',
+        q7: "Check out the High Museum! Free on first sunday of the month if you are a bank of america cardholder! Go on a picnic at Piedmont Park or stop by Sean's for some wraps and sandwiches while there! Go to SCPC events (GT Night at Six Flags, GT Night at the Aquarium, Sting Break, Homecoming Concert, etc.)",
         image: 'IMG_6179.jpeg',
         socials: [
-            {linkedin: 'https://www.linkedin.com'},
-            {linkify: 'ly'},
-            {github: 'g'},
-            {instagram: 'i'},
-            {facebook: 'f'},
-            {twitter: 't'},
-            {tiktok: ''}
+            {linkedin: 'https://www.linkedin.com/in/marissayun/'},
+            {github: 'https://github.com/marissayun'},
+            {instagram: 'https://www.instagram.com/marissayun/'}
         ]
     },
     {
         id: 1,
-        name: 'Name2',
-        major: 'Major',
-        year: 'Year at Tech',
+        name: 'Bernadette Bal',
+        major: 'Computer Science',
+        year: '3rd Year',
         quote: 'Quote',
-        job: 'Job at Tech',
+        job: 'Student Assistant',
         welcome: 'Welcome Message',
         q1: 'words1',
         q2: 'words2',
@@ -42,13 +38,7 @@ export const UserData = [
         q7: 'words7',
         image: 'dummyphoto.png',
         socials: [
-            {linkedin: 'l'},
-            {linkify: 'ly'},
-            {github: 'g'},
-            {instagram: 'i'},
-            {facebook: 'f'},
-            {twitter: 't'},
-            {tiktok: ''}
+            {linkedin: 'https://www.linkedin.com/in/bernadette-bal/'}
         ]
     },
     {
